b8f276e75cf45f5c74f4991ed2d26a3db5520754
Walter E. Rodrigues <111158948+WaltRod@users.noreply.github.com>
2022-10-25 22:41:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function Entrar(){
+    var a = window.document.getElementById('enter')
+    a.style.background = 'rgba(114, 110, 110, 0.281)'
+}
+function Sair(){
+    var a = window.document.getElementById('enter')
+    a.style.background = 'white'
+}
